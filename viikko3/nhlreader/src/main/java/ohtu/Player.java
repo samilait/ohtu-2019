41b259a7ece172f40e608d81075604c97ff8574f
samilait <sami.p.laitinen@gmail.com>
2019-11-18 00:18:47 +0200

    kohta 1 tehty

--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -2,7 +2,8 @@
 package ohtu;
 
 public class Player {
-    private String name;
+    private String name, team;
+    private int goals, assists;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +13,33 @@ public class Player {
         return name;
     }
 
+    public void setTeam(String team) {
+        this.team = team;
+    }
+    
+    public String getTeam() {
+        return team;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+    
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+    
+    public int getAssists() {
+        return assists;
+    }
+
     @Override
     public String toString() {
-        return name;
+        return name + " team " + team + " goals " + goals + " assists " + assists;
     }
       
 }
