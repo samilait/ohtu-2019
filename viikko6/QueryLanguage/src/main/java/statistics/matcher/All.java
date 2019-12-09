09afc95d0225cfd3e99b5cce72c1127697f28d6b
samilait <sami.p.laitinen@gmail.com>
2019-12-09 22:56:20 +0200

    Tehtävä 2: lisättyjä kyselyitä

---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/All.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+import statistics.Player;
+/**
+ *
+ * @author Sami
+ */
+public class All implements Matcher {
+
+    private Matcher[] matchers;
+
+    public All() {
+    }
+    
+    @Override
+    public boolean matches(Player p) {
+        return true;
+    }
+    
+}
